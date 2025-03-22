da59b07af375ef49bb0ac5ba7b6c815f5097c2a2
jeevana05 <jeevanareddy05@gmail.com>
2025-03-22 13:22:50 +0530

    basic home

---
+++ app/steps/step2.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Step2Screen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Step 2: Coming Soon 🚀</Text>
+    </View>
+  );
+}
